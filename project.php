1a4300b7dde67045c31aa63e395202a2762f295f
vithusel <31311292+vithusel@users.noreply.github.com>
2018-07-23 11:42:37 +0100

    Revert "Start merging projects into one folder"
    This reverts commit 64e706064648fae75a155dd5de66ba31ce2b8d5e.

--- project.php
+++ project.php
@@ -33,103 +33,6 @@ include_once "everypage/header.php";
 						</div>
 					</section>
 
-<<<<<<< HEAD
-<style>
-body, php {
-  height: 100%;
-  margin: 0;
-}
-
-.bgimg-1, .bgimg-2, .bgimg-3, .bgimg-4{
-  position: relative;
-  opacity: 0.65;
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-
-}
-.bgimg-1 {
-  background-image: url("images/RaspberryPi.jpg");
-  min-height: 100%;
-}
-
-.bgimg-2 {
-  background-image: url("images/securitysystem.jpg");
-  min-height: 100%;
-}
-
-.caption {
-  position: absolute;
-  left: 0;
-  top: 50%;
-  width: 100%;
-  text-align: center;
-  color: #000;
-}
-
-.caption span.border {
-  background-color: #111;
-  color: #fff;
-  padding: 18px;
-  font-size: 25px;
-  letter-spacing: 10px;
-}
-
-h3 {
-  letter-spacing: 5px;
-  text-transform: uppercase;
-  font: 20px "Lato", sans-serif;
-  color: #111;
-}
-
-/* Turn off parallax scrolling for tablets and phones */
-@media only screen and (max-device-width: 1024px) {
-    .bgimg-1, .bgimg-2, .bgimg-3, .bgimg-4 {
-        background-attachment: scroll;
-    }
-}
-</style>
-</head>
-<body>
-
-<div class="bgimg-1">
-  <div class="caption">
-    <span class="border">Pi Apache</span>
-  </div>
-</div>
-
-<div style="color: #ddd;background-color:#242943;text-align:center;padding:50px 80px;text-align: justify;">
-  <p>This website you are currently on is being run off of a Raspberry pi running Raspbian Apache. The original site is based off of "HTML5UP's" design which i have heavily edited. My hopes for this site is a teaching tool for myself to learn some basic web design languages such as python and java you can see the code for this site on my Github</p>
-</div>
-
-<div class="bgimg-2">
-  <div class="caption">
-    <span class="border" style="font-size:25px;color: #f7f7f7;">Smart Home Security</span>
-  </div>
-</div>
-
-<div style="position:relative;">
-  <div style="color: #ddd;background-color:#242943;text-align:center;padding:50px 80px;text-align: justify;">
-    <p>One of the projects im currently working on it smartifiying my home using the raspberry pi. Although there a plenty of existing solutions available i intend on with starting one from scratch or modifying a existing system to include the following:	</p>
-    <ul>
-	   <li>Nightvision camera</Li>
-       <li>Ability to check the identity of person within the property</Li>
-       <li>Ability to notify my phone when a unauthorised person has been detected</Li>
-       <li>Heat sensor to act as secondary alarm to notify my of fire if no-one is home</Li>
-	</div>
-</div>
-
-
-    <!-- Scripts -->
-      <script src="assets/js/jquery.min.js"></script>
-      <script src="assets/js/jquery.scrolly.min.js"></script>
-      <script src="assets/js/jquery.scrollex.min.js"></script>
-      <script src="assets/js/skel.min.js"></script>
-      <script src="assets/js/util.js"></script>
-      <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-      <script src="assets/js/main.js"></script>
-=======
 				<!-- Main -->
 					<div id="main">
 
@@ -209,7 +112,6 @@ h3 {
 			<script src="assets/js/util.js"></script>
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="assets/js/main.js"></script>	
->>>>>>> parent of 64e7060... Start merging projects into one folder
 <?php
 include_once "everypage/login.php";
 ?>
