f183c9662f00492b1e6559289c1e8f337c604fa3
vithusel <31311292+vithusel@users.noreply.github.com>
2018-07-24 13:59:38 +0100

    add google code

--- index.php
+++ index.php
@@ -10,7 +10,14 @@ include_once "everypage/header.php";
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<!--[if lte IE 8]><script src="assets/js/ie/php5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
-		<link rel="stylesheet" href="assets/css/login.css" />		
+		<link rel="stylesheet" href="assets/css/login.css" />	
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-6697079098987682",
+    enable_page_level_ads: true
+  });
+</script>		
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
