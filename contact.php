914921523e80a801621979cee7e672bf67e2d15a
vithusel <31311292+vithusel@users.noreply.github.com>
2018-07-30 12:30:53 +0100

    remove template form from contacts page

--- contact.php
+++ contact.php
@@ -30,42 +30,6 @@ include_once "everypage/header.php";
 							<div class="content">
 								<p>I am based in Croydon, London.</p>
                                 <p>If you would like to contact me feel free to send me a message on <a href="mailto:vithu@live.com">email</a></p> or via <a href="http://www.linkedin.com/in/vithusel">Linkedin</a></p>
-								<p> or fill in the below form to contact me directly</p>
-								<form action="//submit.form" id="ContactUs100" method="post" onsubmit="return ValidateForm(this);">
-<script type="text/javascript">
-function ValidateForm(frm) {
-if (frm.Name.value == "") { alert('Name is required.'); frm.Name.focus(); return false; }
-if (frm.FromEmailAddress.value == "") { alert('Email address is required.'); frm.FromEmailAddress.focus(); return false; }
-if (frm.FromEmailAddress.value.indexOf("@") < 1 || frm.FromEmailAddress.value.indexOf(".") < 1) { alert('Please enter a valid email address.'); frm.FromEmailAddress.focus(); return false; }
-if (frm.Comments.value == "") { alert('Please enter comments or questions.'); frm.Comments.focus(); return false; }
-return true; }
-</script>
-<table style="width:550px;border:0;color:red;" cellpadding="8" cellspacing="0">
-<tr> <td>
-<label for="Name">Name*:</label>
-</td> <td>
-<input name="Name" type="text" maxlength="60" style="width:250px;" />
-</td> </tr> <tr> <td>
-<label for="PhoneNumber">Phone number:</label>
-</td> <td>
-<input name="PhoneNumber" type="text" maxlength="43" style="width:250px;" />
-</td> </tr> <tr> <td>
-<label for="FromEmailAddress">Email address*:</label>
-</td> <td>
-<input name="FromEmailAddress" type="text" maxlength="90" style="width:250px;" />
-</td> </tr> <tr> <td>
-<label for="Comments">Comments*:</label>
-</td> <td>
-<textarea name="Comments" rows="7" cols="40" style="width:350px;"></textarea>
-</td> </tr> <tr> <td>
-* - required fields
-</td> <td>
-<div style="float:right"><a href="https://www.100forms.com" id="lnk100" title="form to email">form to email</a></div>
-<input name="skip_Submit" type="submit" value="Submit" />
-<script src="https://www.100forms.com/js/FORMKEY:RMNC6QTN6A29" type="text/javascript"></script>
-</td> </tr>
-</table>
-</form>
       							</div>	
 					<footer id="footer">
 						<div class="inner">
