0a8aa2372ef97530f4b81872d4f697900fd33b04
Vithurshan Selvarajah <31311292+vithusel@users.noreply.github.com>
2018-08-07 11:58:14 +0100

    Fix flickering issue on page load

--- index.php
+++ index.php
@@ -9,13 +9,6 @@ include_once "everypage/header.php";
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<!--[if lte IE 8]><script src="assets/js/ie/php5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />	
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-6697079098987682",
-    enable_page_level_ads: true
-  });
-</script>		
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
