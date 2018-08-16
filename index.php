2c035e5479bac18ac5765dbe9d300b1187718068
Vithurshan Selvarajah <31311292+vithusel@users.noreply.github.com>
2018-08-16 23:06:25 +0100

    Revert "try 2"
    This reverts commit e1d84aec93f8265eadfc761f6e5bd36a2b9f7179.

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 <?php
 include_once "everypage/header.php";
 ?>
-	<head onload="myFunction()" style="margin:0;">
-	
-	<div id="loader"></div>
+	<head>
 		<title>Home Page</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
@@ -14,7 +12,9 @@ include_once "everypage/header.php";
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
-	<body> 
+	<body onload="myFunction()" style="margin:0;">
+	
+	<div id="loader"></div>
 
 				<!-- Banner -->
 					<section id="banner" class="major">
