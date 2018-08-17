76b8dd751173cb4226e374b00c1b0fde3b804101
vithusel <31311292+vithusel@users.noreply.github.com>
2018-08-17 11:30:16 +0100

    recentre image

--- project.php
+++ project.php
@@ -152,19 +152,19 @@ include_once "everypage/header.php";
 
                                         <div id="slideshow">
                                            <div>
-                                             <img src="/images/PiStation/1.jpg" style="top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                                             <img src="/images/PiStation/1.jpg" style="margin-left: auto; margin-right: auto;">
                                            </div>
                                            <div>
-                                             <img src="/images/PiStation/2.jpg" style="top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                                             <img src="/images/PiStation/2.jpg" style="margin-left: auto; margin-right: auto;">
                                            </div>
                                            <div>
-                                              <img src="/images/PiStation/3.jpg" style="top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                                              <img src="/images/PiStation/3.jpg" style="margin-left: auto; margin-right: auto;">
                                            </div>
                                            <div>
-                                              <img src="/images/PiStation/4.jpg" style="top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                                              <img src="/images/PiStation/4.jpg" style="margin-left: auto; margin-right: auto;">
                                            </div>
                                            <div>
-                                              <img src="/images/PiStation/5.jpg" style="top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                                              <img src="/images/PiStation/5.jpg" style="margin-left: auto; margin-right: auto;">
                                            </div>										   
                                         </div>
                                         </div>
